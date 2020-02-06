d9dcacf94e5bf4097ae214a16bbe1929113c5c58
Milan Kriz <milan.kriz@eccam.com>
2020-02-06 09:39:38 +0100

    Add 3rdparty Zserio tool and its C++ runtime

---
+++ 3rdparty/runtime/zserio/CppRuntimeVersion.h
@@ -0,0 +1,9 @@
+#ifndef ZSERIO_CPP_RUNTIME_VERSION_H_INC
+#define ZSERIO_CPP_RUNTIME_VERSION_H_INC
+
+/**
+ * C++ extension runtime version string.
+ */
+#define CPP_EXTENSION_RUNTIME_VERSION_STRING "1.4.0-pre2"
+
+#endif // ifndef ZSERIO_CPP_RUNTIME_VERSION_H_INC
